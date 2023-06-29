b9c484a869082de265bef07d16082b693c71c641
OprahAHO <115708026+OprahAHO@users.noreply.github.com>
2023-06-29 20:33:38 +0800

    RotatePlatform

--- Assets/Scripts/PlatformRotation.cs
+++ Assets/Scripts/PlatformRotation.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class PlatformRotation : MonoBehaviour
 {
-    public Transform rotationPlatform;
+    private Transform rotationPlatform;
 
     public Transform PlayerCam;
     public float rotationDuration = 1f; // 旋转持续时间
     public bool canRotate;
     public bool havePlatform;
-    private  RaycastHit hit;
-    private float maxDistance = 1000f;
-
-
+    private RaycastHit hit;
+    //private  RaycastHit hit;
+    //private float maxDistance = 1000f;
 
     private void Start()
     {
         canRotate = true;
         havePlatform = false;
+        
+
     }
     private void Update()
     {
@@ -28,13 +29,29 @@ public class PlatformRotation : MonoBehaviour
 
     void PlatformCheck()
     {
-        havePlatform = (Physics.SphereCast(PlayerCam.position, 50f, PlayerCam.forward*1000, out hit, maxDistance) && hit.collider.GetComponent<RotationPlatformComponent>() != null);
+        //havePlatform = (Physics.SphereCast(PlayerCam.position, 50f, PlayerCam.forward*1000, out hit, maxDistance) && hit.collider.GetComponent<RotationPlatformComponent>() != null);
+        Camera cameraComponent = GameObject.Find("PlayerCam").GetComponent<Camera>();
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        float centerX = screenWidth / 2f;
+        float centerY = screenHeight / 2f;
+        Ray ray = cameraComponent.ScreenPointToRay(new Vector3(centerX,centerY,0f));
+        
+        havePlatform = (Physics.Raycast(ray, out hit, 2000f) && hit.collider.GetComponent<RotationPlatformComponent>() != null);
+        /*if (havePlatform)
+            Debug.Log("have");*/
+        /*if(havePlatform)
+        {
+            Transform collidedTransform = hit.transform;
+            Transform rotationPlatform = collidedTransform.GetComponent<Transform>();
+            Debug.Log("碰撞物体的位置：" + collidedTransform.position);
 
-       
+        }*/
+
     }
     private void Rotatemachine()
     {
-        if (Input.GetKeyDown(KeyCode.I) && canRotate && havePlatform)
+        if (Input.GetKeyDown(KeyCode.F) && canRotate && havePlatform)
             RotateStart();
 
     }
@@ -45,7 +62,8 @@ public class PlatformRotation : MonoBehaviour
 
     IEnumerator RotateCoroutine()
     {
-        Quaternion startRotation = rotationPlatform.rotation; // 初始旋转角度
+        Transform isNewPlatform = hit.transform;
+        Quaternion startRotation = isNewPlatform.rotation; // 初始旋转角度
         Quaternion targetRotation = Quaternion.Euler(0f, 90f, 0f) * startRotation; // 目标旋转角度（向右旋转九十度）
 
         float elapsedTime = 0f;
@@ -53,13 +71,13 @@ public class PlatformRotation : MonoBehaviour
         {
             // 根据时间插值计算当前旋转角度
             float t = elapsedTime / rotationDuration;
-            rotationPlatform.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
+            isNewPlatform.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
 
             elapsedTime += Time.deltaTime;
             yield return null;
         }
         // 确保最终旋转角度准确
-        rotationPlatform.rotation = targetRotation;
+        isNewPlatform.rotation = targetRotation;
         RotateStop();
     }
     void RotateStart()
