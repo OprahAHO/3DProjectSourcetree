15fae43d3cfedb543fa7d4053e60a54198636c42
OprahAHO <115708026+OprahAHO@users.noreply.github.com>
2023-07-26 01:00:38 +0800

    resume

--- Assets/Matt/Scripts/Manager/GameManager.cs
+++ Assets/Matt/Scripts/Manager/GameManager.cs
@@ -50,15 +50,9 @@ public class GameManager : MonoBehaviour
                 Cursor.visible = true;
                 Time.timeScale = 0;
             }
-            else if (Input.GetKeyDown(KeyCode.Escape) && pause)
+            else if(Input.GetKeyDown(KeyCode.Escape) && pause)
             {
-                skill.SetActive(true);
-                pauseCanvas.SetActive(false);
-                pause = false;
-
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-                Time.timeScale = 1;
+                Resume();
             }
         }
         if(Input.GetKeyDown(KeyCode.R) /*&& !live*/)
@@ -70,7 +64,16 @@ public class GameManager : MonoBehaviour
             pause = false;
         }
     }
+     public void Resume()
+     {
+        skill.SetActive(true);
+        pauseCanvas.SetActive(false);
+        pause = false;
 
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+     }
    
     public void GameOver()
     {
@@ -106,7 +109,7 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-    public void Resume()
+    public void Main()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
