590b123a31fb83f27d8e01a6d5b649c4541cd5be
OprahAHO <115708026+OprahAHO@users.noreply.github.com>
2023-08-01 15:16:16 +0800

    la
    la

--- Assets/Scripts/TimeUsed.cs
+++ Assets/Scripts/TimeUsed.cs
@@ -10,11 +10,14 @@ public class TimeUsed : MonoBehaviour
 {
     public Text timeText;
     public Text scenceText;
+    public Text title;
     private string time;
     private int scence;
     public string toturial;
     public string Level1;
     public string Level2;
+    public string titletoturial;
+
 
     void Start()
     {
@@ -29,21 +32,25 @@ public class TimeUsed : MonoBehaviour
 
     void scenceChoice()
     {
-        if(scence == 2)
+        if (scence == 2)
         {
             scenceText.text = toturial;
+            title.text = "H0-" + titletoturial;
         }
-        else if(scence == 3)
+        else if (scence == 3)
         {
             scenceText.text = Level1;
+            title.text = "H0-01";
         }
         else if (scence == 4)
         {
             scenceText.text = Level2;
+            title.text = "H0-02";
         }
         else if (scence == 6)
         {
             scenceText.text = "testScence";
+            title.text = "H0-03";
         }
     }
 }
